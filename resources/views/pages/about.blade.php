1c6f301e40c5dd224040558134dfcfa0d323d48d
Rotherford Marmibe <rmarmibe@gmail.com>
2018-09-05 20:02:04 +0800

    init commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <!--
+    <h1>{{$title}}</h1>   
+    <h1><?php echo $title; ?></h1>   
+    -->
+
+    <h1><?php echo $title; ?></h1>      
+
+    <p>About Page</p>    
+@endsection
