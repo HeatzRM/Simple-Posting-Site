3ab079f8914fd04c9c15c3edf5766fba5fe72d42
Rotherford Marmibe <rmarmibe@gmail.com>
2018-09-06 00:30:08 +0800

    edit and delete posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -22,7 +22,7 @@ class PostsController extends Controller
         //$posts = Post::orderBy('title','desc')->get();
         //$posts = Post::orderBy('title','desc')->take(1)->get(); //chain, getting a # of items
         
-        $posts = Post::orderBy('created_at','desc')->paginate(1); //pagination
+        $posts = Post::orderBy('created_at','desc')->paginate(10); //pagination
         return view('posts.index')->with('posts', $posts);
     }
 
@@ -78,7 +78,8 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.edit')->with('post', $post);
     }
 
     /**
@@ -90,7 +91,18 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+        
+        //Create Post
+        $post = Post::find($id);
+        $post->title = $request->input('title'); //gets whatever is submitted in the form
+        $post->body = $request->input('body');
+        $post->save();
+
+        return redirect('/posts')->with('success', 'Post Updated');
     }
 
     /**
@@ -101,6 +113,8 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/posts')->with('success', 'Post Removed');
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,9 +3,16 @@
 @section('content')
     <a href="/posts" class="btn btn-primary">Go Back</a>  
     <h1>{{$post->title}}</h1>
-    <small>Written on {{$post->created_at}}</small>
 
     <div>
         {!!$post->body!!}
     </div>
+    <hr>
+    <small>Written on {{$post->created_at}}</small>
+    <hr>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-primary">Edit</a>
+    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method'=>'POST', 'class'=>'pull-right'])!!}
+        {{Form::hidden('_method', 'Delete')}}
+        {{Form::submit('Delete', ['class'=>"btn btn-danger"])}}
+    {!!Form::close()!!}
 @endsection
